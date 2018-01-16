05cb848ab4a4d85a75320049f5be1cffdf87cd8a
Pradeep Sanjaya <sanjayangp@gmail.com>
2018-01-16 14:14:59 +0530

    Add query string status

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var url = require('url');
 
 //create a server object:
 http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    var query = parsedUrl.query;
+    if (query.status != undefined) {
+        console.log(query.status);
+    }
     res.write(Date.now().toString());
     res.end();
 }).listen(8080);
